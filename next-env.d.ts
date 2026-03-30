b22072cb6977926f173c543a042ed4d430ef05ae
evialbert <evialbert@gmail.com>
2026-03-30 22:47:45 +0300

    Update: Am schimbat titlul paginii principale

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/navigation" />
 /// <reference types="next/image-types/global" />
 
-// Nu edita acest fișier manual.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
