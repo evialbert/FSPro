3d2a682f4478b27f9686869959a55cda81662d61
evialbert <evialbert@gmail.com>
2026-04-23 16:41:29 +0300

    Fix tailwind config syntax error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
 import type { Config } from "tailwindcss";
 
-const config: Config {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Aceasta linie este esențială!
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        // Aici poți defini culorile tale personalizate dacă vrei
         brand: "#003366",
       },
     },
